0591df6f55f064de67ced92bb4ce37a62b71c399
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-05-12 13:29:22 +0200

    Lectura HTML

--- ejercicios/Módulo 2 - Visualización de datos/04_lectura_db_con_pandas.py
+++ ejercicios/Módulo 2 - Visualización de datos/04_lectura_db_con_pandas.py
@@ -7,10 +7,22 @@ conn = sqlite3.connect('db_empleados_optimizado.sqlite3')
 # df = pd.read_sql_query('SELECT * FROM empleados', conn) # Todos los campos de todos los registros
 # df = pd.read_sql_query('SELECT id, categoria_programacion, ciudad, salario FROM empleados', conn) # Obtenemos solo algunos campos
 # df = pd.read_sql_query('SELECT categoria_programacion, salario FROM empleados WHERE ciudad="Madrid"', conn)
+
 # Empleados de Madrid que cobren más de 30_000
+# df = pd.read_sql_query('SELECT id as identificador, categoria_programacion, salario FROM empleados WHERE ciudad="Madrid" AND salario>30000', conn)
+
 # Empleados de Madrid que sean Científico de Datos
-# Empleados de Madrid y de Barcelona que sean DevOps Engineer
+# df = pd.read_sql_query('SELECT * FROM empleados WHERE ciudad="Madrid" AND categoria_programacion="Científico de Datos"', conn)
+
+# Empleados de Madrid que sean desarrolladores (da igual de front o de back)
+# df = pd.read_sql_query('SELECT * FROM empleados WHERE ciudad="Madrid" AND categoria_programacion LIKE "%Desarrollador%"', conn)
+
+# Empleados de Madrid o de Barcelona que sean DevOps Engineer
+# df = pd.read_sql_query('SELECT * FROM empleados WHERE (ciudad="Madrid" OR ciudad="Barcelona") AND categoria_programacion="DevOps Engineer"', conn)
+
 # Empleados que sean DevOps Engineer y que cobren menos de 30_000
+df = pd.read_sql_query('SELECT * FROM empleados WHERE categoria_programacion="DevOps Engineer" AND salario<30000', conn)
+
 print(df.info())
 print(df.head())
 print(df.tail())
---
+++ ejercicios/Módulo 2 - Visualización de datos/05_lectura_html_con_pandas.py
@@ -0,0 +1,15 @@
+import requests
+from io import StringIO
+
+import pandas as pd
+
+URL = 'https://es.wikipedia.org/wiki/Liga_Nacional_de_F%C3%BAtbol_Profesional'
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+html = requests.get(URL, headers=headers).text
+html_io = StringIO(html)
+data_frames = pd.read_html(html_io)
+
+for data_frame in data_frames:
+    print(data_frame.info())
